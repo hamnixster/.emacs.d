f20d12393cce31b54677d0ca2bef3fb9f573f8fc
Graham Bouvier <hamnixster@gmail.com>
2020-07-18 14:01:10 -0700

    Add a inf-ruby function to directly send a string
    It's worth noting here that this (and other inf-ruby function) use inf-ruby-first-prompt-pattern which will requrie pry
    to be enabled given the other configuration above.

--- modules/programming/grm-ruby.el
+++ modules/programming/grm-ruby.el
@@ -12,4 +12,29 @@
 
 (advice-add 'inf-ruby-console-auto :before #'chruby-use-corresponding)
 
+(setq inf-ruby-console-environment "development")
+
+(require 'inf-ruby)
+(defun ruby-send-string (str)
+  "Send a string to the inf-ruby-proc and return the output as an elisp string"
+  (if (not (inf-ruby-buffer)) ""
+    (comint-send-string (inf-ruby-proc) (format "%s\n" str))
+    (let ((proc (inf-ruby-proc)))
+      (with-current-buffer (or (inf-ruby-buffer)
+                               inf-ruby-buffer)
+        (while (not (and comint-last-prompt
+                         (goto-char (car comint-last-prompt))
+                         (looking-at inf-ruby-first-prompt-pattern)))
+          (accept-process-output proc))
+        (re-search-backward inf-ruby-prompt-pattern)
+        (or (re-search-forward " => " (car comint-last-prompt) t)
+            ;; Evaluation seems to have failed.
+            ;; Try to extract the error string.
+            (let* ((inhibit-field-text-motion t)
+                   (s (buffer-substring-no-properties (point) (line-end-position))))
+              (while (string-match inf-ruby-prompt-pattern s)
+                (setq s (replace-match "" t t s)))
+              (error "%s" s)))
+        (buffer-substring-no-properties (point) (line-end-position))))))
+
 (provide 'grm-ruby)
