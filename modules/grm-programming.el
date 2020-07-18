0aa3a71c828b1f0a6c69e4d9e34c99a2e9537061
Graham Bouvier <hamnixster@gmail.com>
2020-07-18 14:02:56 -0700

    Add some crazy rails helpers to help with snippets

--- modules/grm-programming.el
+++ modules/grm-programming.el
@@ -1,5 +1,6 @@
 (add-to-list 'load-path (concat grm-modules-dir "/programming"))
 
 (grm-require 'grm-ruby)
+(grm-require 'grm-rails)
 
 (provide 'grm-programming)
---
+++ modules/programming/grm-rails.el
@@ -0,0 +1,30 @@
+(defun rails-classify-file ()
+  (string-trim
+   (ruby-send-string
+    (format "'%s'.split(/#{Rails.root}\\/app\\//).last.split('/', 2).last.classify"
+            (file-name-sans-extension (buffer-file-name)))) "\"" "\""))
+
+(defun rails-classify-file-modules ()
+  (string-join
+   (mapcar (lambda (x) (format "module %s" x))
+           (split-string (rails-classify-file) "::"))
+   "\n"))
+
+(defun rails-classify-file-modules-class ()
+  (let* ((mods (split-string (rails-classify-file) "::"))
+         (modules (butlast mods))
+         (class (car (reverse mods))))
+    (string-join
+     (append
+      (mapcar (lambda (x) (format "module %s" x)) modules)
+      (list (format "class %s" class)))
+     "\n")))
+
+(defun rails-classify-file-modules-ends ()
+  (string-join
+   (make-list
+    (length (split-string (rails-classify-file) "::"))
+    "end")
+   "\n"))
+
+(provide 'grm-rails)
