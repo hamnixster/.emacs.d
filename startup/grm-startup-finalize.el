d6b9ac3839e3e58fe4e7c1e98af2793029b738d8
Graham Bouvier <hamnixster@gmail.com>
2020-07-18 18:34:22 -0700

    Update start screen

--- startup/grm-startup-finalize.el
+++ startup/grm-startup-finalize.el
@@ -1,7 +1,19 @@
 (defvar grm-init-duration (float-time (time-since grm-before-init-time)))
 
-(setq initial-major-mode 'org-mode
-      initial-scratch-message
-      (format "* scratch"))
+(defun random-choice (items)
+  (let* ((size (length items))
+         (index (random size)))
+    (nth index items)))
+
+(let ((quotes
+       '("There's a good part of Computer Science that's like magic.\n  Unfortunately there's a bad part of Computer Science that's like religion."
+         "If I have not seen as far as others, it is because giants were standing on my shoulders."
+         "The computer revolution is a revolution in the way we think and in the way we express what we think."
+         "Nothing brings fear to my heart more than a floating point number."
+         "We can only see a short distance ahead, but we can see plenty there that needs to be done."
+         "One day ladies will take their computers for walks in the park and tell each other,\n  \"My little computer said such a funny thing this morning\".")))
+  (setq initial-major-mode 'org-mode
+        initial-scratch-message
+        (format "* %s \n  %s\n" (format-time-string "%Y-%m-%d") (random-choice quotes))))
 
 (provide 'grm-startup-finalize)
