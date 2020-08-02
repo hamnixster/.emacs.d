5b358e3746b69bedb1d181fa773a17ecfab1fdeb
Graham Bouvier <hamnixster@gmail.com>
2020-08-01 18:35:07 -0700

    Make beacon longer

--- modules/features/grm-feature-beacon.el
+++ modules/features/grm-feature-beacon.el
@@ -3,7 +3,7 @@
   (add-to-list 'beacon-dont-blink-major-modes mode))
 (setq beacon-blink-when-point-moves-vertically 10
       beacon-blink-when-buffer-changes t
-      beacon-size 60
+      beacon-size 140
       beacon-color grm-whitespace-background-color)
 
 (provide 'grm-feature-beacon)
